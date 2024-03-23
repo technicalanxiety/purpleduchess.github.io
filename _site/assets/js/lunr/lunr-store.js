03572079b8f091cbb58f3369b852694d75ef461d
Jason Rinehart <jason.rinehart@technicalanxiety.com>
2024-03-23 15:35:25 -0500

    setting up the site and first example post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "The first post",
+        "excerpt":" Example      ;lkjasdfopi ;lkajsd;lkieifj oj;lkjadsiofjk oij;lkjwoeifjij   **Photo by [](https://unsplash.com/) on [Unsplash](https://unsplash.com/photos/)**  ","categories": [],
+        "tags": [],
+        "url": "/template/",
+        "teaser": null
+      }]
